0b7460d3772995c8d5edd7f6465511ba744a6380
Brian Kelley <brian.kelley@jamf.com>
2020-04-09 15:18:50 -0500

    Update code with :allthethings:

--- rds-snapshotter-orca/src/main/kotlin/oss/plugin/stage/snapshotter/rds/RDSInput.kt
+++ rds-snapshotter-orca/src/main/kotlin/oss/plugin/stage/snapshotter/rds/RDSInput.kt
@@ -4,4 +4,5 @@ package oss.plugin.stage.snapshotter.rds
  * This the the part of the Context map that we care about as input to the stage execution.
  * The data can be key/value pairs or an entire configuration tree.
  */
-data class RDSInput(var maxWaitTime: Int) {}
+data class RDSInput(var awsAccount: String, var instanceName: String, var snapshotName: String, var region: String) {}
+
